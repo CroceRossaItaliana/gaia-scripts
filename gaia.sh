a1b7cb814e5163054059827af66e25b6f81e604f
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2013-10-01 19:55:18 +0100

    Aggiornamento, migliorie varie e build-essential

--- gaia.sh
+++ gaia.sh
@@ -18,6 +18,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 shopt -s expand_aliases
+
+# Se e' la prima esecuzione crea .aliasgaia
+if [ ! -f ~/.aliasgaia ];then
+        echo "GAIALOCATION=/home/$USER/gaia" > ~/.aliasgaia
+fi
+        
 source ~/.aliasgaia
 
 if [[ "$1" = "install" ]];then
@@ -35,7 +41,7 @@ if [[ "$1" = "install" ]];then
         echo " "
         sudo add-apt-repository --yes ppa:ondrej/php5
         sudo apt-get update
-        sudo apt-get install --yes git wget sed unzip nano php5-cli php5-common php-pear php-mail mysql-server php5-dev php5-mysql redis-server
+        sudo apt-get install --yes git wget build-essential sed unzip nano php5-cli php5-common php-pear php-mail mysql-server php5-dev php5-mysql redis-server
         sudo service mysql start
 
         clear
@@ -65,7 +71,7 @@ if [[ "$1" = "install" ]];then
         fi
 
         if [ ! -d $location ];then
-         mkdir -p $location
+                mkdir -p $location
         fi
         
         echo "GAIALOCATION=$gaialocation" > ~/.aliasgaia
@@ -95,24 +101,23 @@ if [[ "$1" = "install" ]];then
         echo "Inserisci la password di root di MySQL"
         read -s pmysql 
         echo "Creazione database 'gaia'..."
-        echo "CREATE DATABASE gaia;" | mysql -u root --password=$pmysql
+        echo "CREATE DATABASE gaia;" | mysql -u root --password="$pmysql"
         echo "Creazione utente 'gaia'..."
-        echo "GRANT ALL ON gaia.* TO gaia@localhost IDENTIFIED BY '$pmysql';" | mysql -u root --password=$pmysql
-        echo "FLUSH PRIVILEGES;" | mysql -u root --password=$pmysql
+        echo "GRANT ALL ON gaia.* TO gaia@localhost IDENTIFIED BY '$pmysql';" | mysql -u root --password="$pmysql"
+        echo "FLUSH PRIVILEGES;" | mysql -u root --password="$pmysql"
         echo "Importazione database..."
-        cat core/conf/gaia.sql | mysql -u gaia --password=$pmysql --database=gaia
+        cat core/conf/gaia.sql | mysql -u gaia --password="$pmysql" --database=gaia
         
         echo " "
-        echo "Vuoi installare i dati di esempio o ti arrangi a manina? [s]/n"
+        echo "Vuoi installare dei comitati di esempio? s/[n]"
         read -n 1 -s datiesempio
         if [[ "$datiesempio" = "s" ]];then
                 # creazione primo comitato di esempio altrimenti è un casino!
-                cat "INSERT INTO `gaia`.`nazionali` (`id`, `nome`, `geo`) VALUES ('1', 'Comitato Nazionale', GeomFromText('POINT(1 2)',0));" | mysql -u gaia --password=$pmysql --database=gaia
-                cat "INSERT INTO `gaia`.`regionali` (`id`, `nome`, `geo`, `nazionale`) VALUES ('1', 'Regionale prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password=$pmysql --database=gaia
-                cat "INSERT INTO `gaia`.`provinciali` (`id`, `nome`, `geo`, `regionale`) VALUES ('1', 'provinciale di prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password=$pmysql --database=gaia
-                cat "INSERT INTO `gaia`.`locali` (`id`, `nome`, `geo`, `provinciale`) VALUES ('1', 'locale di prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password=$pmysql --database=gaia
-                cat "INSERT INTO `gaia`.`comitati` (`id`, `nome`, `colore`, `locale`, `geo`, `principale`) VALUES ('1', 'comitato locale di prova', NULL, '1', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password=$pmysql --database=gaia
-                #
+                cat "INSERT INTO `gaia`.`nazionali` (`id`, `nome`, `geo`) VALUES ('1', 'Comitato Nazionale', GeomFromText('POINT(1 2)',0));" | mysql -u gaia --password="$pmysql" --database=gaia
+                cat "INSERT INTO `gaia`.`regionali` (`id`, `nome`, `geo`, `nazionale`) VALUES ('1', 'Regionale prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password="$pmysql" --database=gaia
+                cat "INSERT INTO `gaia`.`provinciali` (`id`, `nome`, `geo`, `regionale`) VALUES ('1', 'provinciale di prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password="$pmysql" --database=gaia
+                cat "INSERT INTO `gaia`.`locali` (`id`, `nome`, `geo`, `provinciale`) VALUES ('1', 'locale di prova', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password="$pmysql" --database=gaia
+                cat "INSERT INTO `gaia`.`comitati` (`id`, `nome`, `colore`, `locale`, `geo`, `principale`) VALUES ('1', 'comitato locale di prova', NULL, '1', GeomFromText('POINT(1 2)',0), '1');" | mysql -u gaia --password="$pmysql" --database=gaia
         fi
         echo "Creazione configurazione..."
         cp core/conf/database.conf.php.sample core/conf/database.conf.php
@@ -124,9 +129,12 @@ if [[ "$1" = "install" ]];then
         sed -i "s/DATABASE_PASSWORD/$pmysql/g" core/conf/database.conf.php
 
 
+        # Avvia il server per il setup su una porta diversa
+        php -S localhost:8889 > /dev/null 2>&1 # Muto!
         clear
         echo "OK! Installazione di Gaia quasi completa"
-        echo "==> http://localhost:8888/setup.php <== per terminare"
+        echo "Verra' ora avviato il setup di Gaia all'indirizzo"
+        echo "==> http://localhost:8889/setup.php <=="
         echo " "
         echo "In futuro, per avviare Gaia, aprire un terminale e digitare:"
         echo "  gaia.sh start"
@@ -134,10 +142,9 @@ if [[ "$1" = "install" ]];then
         echo "In futuro, per avviare phpMyAdmin, aprire un terminale e digitare:"
         echo "  gaia.sh startdb"
         echo " "
-        echo "Avvio di Gaia... Premi invio quando sei pronto (refreshare il browser)"
+        echo "Avvio di Gaia (setup)... Premi INVIO quando sei pronto"
         read -n 1 -s
-        sensible-browser localhost:8888 &
-        php -S localhost:8888
+        sensible-browser "http://localhost:8889/setup.php" &
 
 elif [[ "$1" = "uninstall" ]];then
         clear
@@ -170,13 +177,32 @@ elif [[ "$1" = "uninstall" ]];then
         clear
         
 elif [[ "$1" = "start" ]];then
-        sensible-browser localhost:8888 &
-        gaia
+        gaia &
+        clear
+        echo "Gaia avviato su http://localhost:8888/"
+        echo "Aprire un browser? [s]/n"
+        read -n 1 -s aprirebrowser
+        if [[ "$aprirebrowser" != "n" ]];then
+                sensible-browser localhost:8888 &
+        fi
+        
 elif [[ "$1" = "startdb" ]];then
-        sensible-browser localhost:8887 &
-        pma
+        gaia &
+        pma &
+        clear
+        echo "Gaia        avviato su http://localhost:8888/"
+        echo "phpMyAdmin  avviato su http://localhost:8887/"
+        echo "Aprirli in un browser un browser? [s]/n"
+        read -n 1 -s aprirebrowser
+        if [[ "$aprirebrowser" != "n" ]];then
+                sensible-browser localhost:8888 &
+                sensible-browser localhost:8887 &
+        fi
+        
 elif [[ "$1" = "stop" ]];then
-        ps aux | grep [p]hp | awk '{print $2}' | xargs kill
+        echo "Chiusura Gaia e PMA..."
+        killall php
+        
 else
         echo "gaia.sh install | uninstall | start | startdb | stop"
         echo ""
