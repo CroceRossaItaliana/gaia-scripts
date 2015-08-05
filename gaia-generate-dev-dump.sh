e0d463fedbbb786b694cfe34235b60f3a718793b
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2015-08-05 14:29:00 +0100

    Update gaia-generate-dev-dump.sh

--- gaia-generate-dev-dump.sh
+++ gaia-generate-dev-dump.sh
@@ -42,62 +42,71 @@ if [[ "$1" ]];then
     db="temp$RANDOM"
 
     echo "- Creazione database temporaneo ($db)..."
-    echo "CREATE DATABASE $db" | mysql -u root --password="$mypassword"
+    echo "CREATE DATABASE $db" | MYSQL_PWD=$mypassword mysql -u root 
 
     echo "- Caricamento DUMP originale in memoria..."
-    mysql -u root --password="$mypassword" $db < $1
+    MYSQL_PWD=$mypassword mysql -u root $db < $1
 
     echo "- Oscuro i nomi..."
-    echo "UPDATE anagrafica SET nome = CONCAT('Nome ', id)  WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET nome = CONCAT('Nome ', id)  WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Oscuro i cognomi..."
-    echo "UPDATE anagrafica SET cognome = CONCAT('Cognome ', id)  WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET cognome = CONCAT('Cognome ', id)  WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Oscuro le password..."
-    echo "UPDATE anagrafica SET password = '253903f72bbb236fe1cc72dd034b4e78061d9d57' WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET password = '253903f72bbb236fe1cc72dd034b4e78061d9d57' WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Oscuro i codici fiscali..."
-    echo "UPDATE anagrafica SET codiceFiscale = CONCAT(LEFT(codiceFiscale, 10), 'X1234X') WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
-    echo "UPDATE anagrafica SET codiceFiscale = CONCAT('ABCDED', RIGHT(codiceFiscale, 10)) WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET codiceFiscale = CONCAT(LEFT(codiceFiscale, 10), 'X1234X') WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword  mysql -u root $db
+    echo "UPDATE anagrafica SET codiceFiscale = CONCAT('ABCDED', RIGHT(codiceFiscale, 10)) WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Oscuro le email..."
-    echo "UPDATE anagrafica SET email = CONCAT('XXXXX', RIGHT(email, length(email)-5)) WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET email = CONCAT('XXXXX', RIGHT(email, length(email)-5)) WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
     
     echo "- Oscuro i telefoni..."
-    echo "UPDATE dettagliPersona SET valore = CONCAT('XXXX', RIGHT(valore, length(valore)-4)) WHERE nome like '%cellulare%' OR nome like '%cellulareServizio%' "  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE dettagliPersona SET valore = CONCAT('XXXX', RIGHT(valore, length(valore)-4)) WHERE nome like '%cellulare%' OR nome like '%cellulareServizio%' "  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Oscuro dettagli anagrafici..."
-    echo "UPDATE dettagliPersona SET valore = CONCAT(nome, id) WHERE id NOT IN (SELECT id FROM anagrafica WHERE admin > 0) AND valore <> '' AND valore NOT REGEXP '[0-9]+';"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE dettagliPersona SET valore = CONCAT(nome, id) WHERE id NOT IN (SELECT id FROM anagrafica WHERE admin > 0) AND valore <> '' AND valore NOT REGEXP '[0-9]+';"  | MYSQL_PWD=$mypassword  mysql -u root $db
 
     echo "- Oscuro il sesso..."
-    echo "UPDATE anagrafica SET sesso = 0  WHERE admin = 0 OR admin IS NULL"  | mysql -u root --password="$mypassword" $db
+    echo "UPDATE anagrafica SET sesso = 0  WHERE admin = 0 OR admin IS NULL"  | MYSQL_PWD=$mypassword mysql -u root $db
     
-    echo "- Oscuro la posta..."
-    echo "UPDATE email SET corpo = 'Oscurato'"  | mysql -u root --password="$mypassword" $db
+    echo "- Cancello la posta..."
+    echo "DELETE FROM email"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM email_allegati"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM email_destinatari"  | MYSQL_PWD=$mypassword mysql -u root $db
+    
+    echo "- Cancello dati donazioni sangue..."
+    echo "DELETE FROM donazioni"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM donazioni_anagrafica"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM donazioni_merito"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM donazioni_personale"  | MYSQL_PWD=$mypassword mysql -u root $db
+    echo "DELETE FROM donazioni_sedi"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Cancello le sessioni..."
-    echo "DELETE FROM sessioni"  | mysql -u root --password="$mypassword" $db
+    echo "DELETE FROM sessioni"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Cancello le sessioni (dettagli)..."
-    echo "DELETE FROM datiSessione"  | mysql -u root --password="$mypassword" $db
+    echo "DELETE FROM datiSessione"  | MYSQL_PWD=$mypassword mysql -u root $db
     
     echo "- Cancello i file..."
-    echo "DELETE FROM file"  | mysql -u root --password="$mypassword" $db
+    echo "DELETE FROM file"  | MYSQL_PWD=$mypassword mysql -u root $db
     
     echo "- Cancello gli avatar..."
-    echo "DELETE FROM avatar"  | mysql -u root --password="$mypassword" $db
+    echo "DELETE FROM avatar"  | MYSQL_PWD=$mypassword mysql -u root $db
     
     echo "- Cancello gli allegati..."
-    echo "DELETE FROM email_allegati"  | mysql -u root --password="$mypassword" $db
+    echo "DELETE FROM email_allegati"  | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo "- Esportazione del file..."
-    mysqldump -u root --password="$mypassword" $db > output.sql
+    MYSQL_PWD=$mypassword mysqldump -u root $db > output.sql
 
     echo "- Compressione del dump..."
     bzip2 --best -f output.sql
 
     echo "- Cancello il database temporaneo ($db)..."
-    echo "DROP DATABASE $db" | mysql -u root --password="$mypassword"
+    echo "DROP DATABASE $db" | MYSQL_PWD=$mypassword mysql -u root $db
 
     echo " " 
     echo "Generato output.sql.gz"
